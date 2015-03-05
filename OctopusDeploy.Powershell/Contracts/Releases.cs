39c6c1300891fc1a1c270f0942fabf67069e6c9f
Nir Weinberg <nir.weinberg@didata.com.au>
2015-03-05 17:02:50 +1100

    Adding a new Cmdlet - Get-OctoReleses which is based on the existing one - Get-OctoRelease - to retrieve a list of releases for a project

---
+++ OctopusDeploy.Powershell/Contracts/Releases.cs
@@ -0,0 +1,39 @@
+﻿namespace DD.Cloud.OctopusDeploy.Powershell.Contracts
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The data contract for a releases
+    /// </summary>
+    public class Releases
+    {
+        public string ItemType
+        {
+            get;
+            set;
+        }
+
+        public bool IsStale
+        {
+            get;
+            set;
+        }
+
+        public int TotalResults
+        {
+            get;
+            set;
+        }
+
+        public int ItemsPerPage
+        {
+            get;
+            set;
+        }
+        public List<Release> Items
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ OctopusDeploy.Powershell/GetOctoReleases.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DD.Cloud.OctopusDeploy.Powershell
+{
+    using System.Management.Automation;
+    using System.Net;
+    using RestSharp;
+
+    [Cmdlet(VerbsCommon.Get, "OctoReleases", ConfirmImpact = ConfirmImpact.Low)]
+    public class GetOctoReleases : OctoCmdlet
+    {
+        /// <summary>
+        /// The project the release belongs to.
+        /// </summary>
+        [Parameter(Mandatory = true, ValueFromPipeline = true, ParameterSetName = "GetOctoReleasesByProject")]
+        public Contracts.Project Project
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The project the release belongs to.
+        /// </summary>
+        [Parameter(Mandatory = true, ParameterSetName = "GetOctoReleasesByProjectId")]
+        public string ProjectId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///		Asynchronously perform Cmdlet processing.
+        /// </summary>
+        /// <returns>
+        ///		A <see cref="Task"/> representing the asynchronous operation.
+        /// </returns>
+        protected override async Task ProcessRecordAsync()
+        {
+            var client = new RestClient(BaseUri);
+
+            string projectId = string.Empty;
+            switch (ParameterSetName)
+            {
+                case "GetOctoReleasesByProjectId":
+                {
+                    projectId = ProjectId;
+                    break;
+                }
+                case "GetOctoReleasesByProject":
+                {
+                    projectId = Project.Id;
+                    break;
+                }
+            }
+
+            var request = new RestRequest("/api/projects/{projectId}/releases/", Method.GET);
+            request.AddHeader("X-Octopus-ApiKey", ApiKey);
+            request.AddUrlSegment("projectId", projectId);
+            var response = await client.ExecuteTaskAsync<Contracts.Releases>(request);
+        
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                WriteError(new ErrorRecord(new Exception(response.ErrorMessage ?? response.Content), "Failed", ErrorCategory.OpenError, null));
+            }
+            else
+            {
+                WriteObject(response.Data);
+            }
+        }
+    }
+}
