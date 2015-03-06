4d5f9cb74d04100b3d7de22b6c9012bcc4a67695
Nir Weinberg <nir.weinberg@didata.com.au>
2015-03-06 13:59:56 +1100

    Adding Set-OctoRelease Cmdlet to allow updating a release - exposing only release notes at the moment to allow adding/editing release notes of a given release

---
+++ OctopusDeploy.Powershell/SetOctoRelease .cs
@@ -0,0 +1,126 @@
+﻿namespace DD.Cloud.OctopusDeploy.Powershell
+{
+    using System;
+    using System.Management.Automation;
+    using System.Net;
+    using System.Threading.Tasks;
+    using Contracts;
+    using RestSharp;
+    using Task = System.Threading.Tasks.Task;
+
+    [Cmdlet(VerbsCommon.Set, "OctoRelease", ConfirmImpact = ConfirmImpact.Medium)]
+    public class SetOctoRelease : OctoCmdlet
+    {
+        /// <summary>
+        /// The version of the release to update 
+        /// </summary>
+        [Parameter(Mandatory = true)]
+        [Parameter(ParameterSetName = "SetOctoReleaseByProject")]
+        [Parameter(ParameterSetName = "SetOctoReleaseByProjectId")]
+        public Version Version
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The project the release belongs to.
+        /// </summary>
+        [Parameter(Mandatory = true, ValueFromPipeline = true, ParameterSetName = "SetOctoReleaseByProject")]
+        public Contracts.Project Project
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The project the release belongs to.
+        /// </summary>
+        [Parameter(Mandatory = true, ParameterSetName = "SetOctoReleaseByProjectId")]
+        public string ProjectId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The release notes to set.
+        /// </summary>
+        [Parameter(Mandatory = false)]
+        public string ReleaseNotes
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///		Asynchronously perform Cmdlet processing.
+        /// </summary>
+        /// <returns>
+        ///		A <see cref="System.Threading.Tasks.Task"/> representing the asynchronous operation.
+        /// </returns>
+        protected override async Task ProcessRecordAsync()
+        {
+            var client = new RestClient(BaseUri);
+
+            string projectId = string.Empty;
+
+            switch (ParameterSetName)
+            {
+                case "SetOctoReleaseByProjectId":
+                {
+                    projectId = ProjectId;
+                    break;
+                }
+                case "SetOctoReleaseByProject":
+                {
+                    projectId = Project.Id;
+                    break;
+                }
+            }
+
+            var release = await GetReleaseAsync(client, Version, projectId);
+            if (release.Id != null)
+            {
+                var request = new RestRequest("/api/releases/{id}", Method.PUT);
+                request.AddUrlSegment("id", release.Id);
+                request.AddHeader("X-Octopus-ApiKey", ApiKey);
+
+                request.AddJsonBody(new Contracts.Release
+                {
+                    ProjectId = projectId,
+                    Id = release.Id,
+                    Version = Version.ToString(),
+                    SelectedPackages = release.SelectedPackages,
+                    ReleaseNotes = string.IsNullOrEmpty(ReleaseNotes) ? release.ReleaseNotes : ReleaseNotes
+                });
+
+                var response = await client.ExecuteTaskAsync<Contracts.Release>(request);
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    WriteError(new ErrorRecord(new Exception(response.ErrorMessage ?? response.Content), "Failed",
+                        ErrorCategory.OpenError, null));
+                }
+                else
+                {
+                    WriteObject(response.Data);
+                }
+            }
+        }
+
+        private async Task<Release> GetReleaseAsync(RestClient client, Version version, string projectId)
+        {
+            var request = new RestRequest("/api/projects/{projectId}/releases/{version}", Method.GET);
+            request.AddHeader("X-Octopus-ApiKey", ApiKey);
+            request.AddUrlSegment("projectId", projectId);
+            request.AddUrlSegment("version", version.ToString());
+
+            var response = await client.ExecuteTaskAsync<Contracts.Release>(request);
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                WriteError(new ErrorRecord(new Exception(response.ErrorMessage ?? response.Content), "Failed", ErrorCategory.OpenError, null));
+            }
+            return response.Data;
+        }
+    }
+}
